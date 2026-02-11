dc9c259bf70e2a3c7ccea2592fa37a009ae379e4
sergiobr <sergio.rodrigues@indaiatuba.sp.gov.br>
2026-02-11 07:22:19 -0300

    remover anytype

--- src/pages/media/home/index.tsx
+++ src/pages/media/home/index.tsx
@@ -1,17 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
-
-import { anytypeTest } from "@/lib/querry/anytype";
-
 const MediaHomePage = () => {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["anytypeTest"],
-    queryFn: () => anytypeTest(),
-  });
-
-  console.log(data)
-
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <div className="w-full h-full flex justify-center items-center">
